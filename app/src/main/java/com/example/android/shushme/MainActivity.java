f5e57cc6539277d39fb606ab7e1034aca0b4ac9c
asengsaragih <aldiwahyu.saragih@gmail.com>
2019-11-18 14:07:48 +0700

    adding gradle google place initial place sdk intent to place, tapi data belum diolah

--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -16,10 +16,22 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     // Member variables
     private PlaceListAdapter mAdapter;
     private RecyclerView mRecyclerView;
+
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 10;
 
     /**
      * Called when the activity is starting
@@ -46,6 +60,28 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new PlaceListAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
 
+        // initialize PLACE API KEY
+        initAPIKEY();
+
     }
+
+    private void initAPIKEY() {
+        String API_KEY = "AIzaSyBcwcvlNZZwDFOjrZfOve_4ROIXi3R4W4M";
+        // Initialize the SDK
+        Places.initialize(getApplicationContext(), API_KEY);
+        // Create a new Places client instance
+        PlacesClient placesClient = Places.createClient(this);
+    }
+
+    public void addLocation(View view) {
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS);
+
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this);
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
 
 }
